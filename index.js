6fa08a84b0790419e398dbd543a0ecbabc1b9862
Zainab Azzam <zainab_azzam@yahoo.com>
2020-06-13 02:27:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+// Your code 
+class Cat {
+  constructor(name,sex){
+    console.log(`${name} says meow!`)
+  }
+}
+class Dog{
+  constructor(name,sex){
+    console.log(`${name} says woof!`)
+  }
+}
+console
